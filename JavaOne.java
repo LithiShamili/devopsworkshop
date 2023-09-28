57872dd264d204822c9b85c90fa7b2de721c2d95
LithiShamili <142906124+LithiShamili@users.noreply.github.com>
2023-09-28 15:58:59 +0530

    Add files via upload

---
+++ JavaOne.java
@@ -0,0 +1,7 @@
+class JavaOne
+{
+public static void main(String args[])
+{
+System.out.println("JavaOne");
+}
+}
---
+++ JavaTwo.java
@@ -0,0 +1,7 @@
+class JavaTwo
+{
+public static void main(String args[])
+{
+System.out.println("JavaTwo");
+}
+}
